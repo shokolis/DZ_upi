acdc0a9db8c69b567da5e4e8d10ad077390899ae
ShokoLis <minestrong12@gmail.com>
2023-06-23 23:52:53 +0300

    v1.0.0 dz_upi2

---
+++ dz_upi2.py
@@ -0,0 +1,21 @@
+data_list = []
+
+while True:
+    fio = input("Введите ФИО преподавателя (для выхода введите 'q'): ")
+    if fio == 'q':
+        break
+    predmet = input("Введите название предмета: ")
+    otsenka = input("Введите оценку: ")
+    data_zacheta = input("Введите дату зачёта: ")
+
+    data_list.append({'ФИО': fio, 'Предмет': predmet, 'Оценка': otsenka, 'Дата зачёта': data_zacheta})
+
+    print("Данные успешно добавлены в список.")
+
+print("Введенные данные:")
+
+for data in data_list:
+    print(data['ФИО'])
+
+print("Сохраненные данные:")
+print(data_list)
